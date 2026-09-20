413319d42ac993e0e8efe26b1c3af9300026f88d
men123-debug <menna0166@gmail.com>
2026-09-20 19:06:11 +0200

    Create const.py

---
+++ custom_components/entfeuchter_preisregler/custom_components/entfeuchter_preisregler/const.py
@@ -0,0 +1,19 @@
+"""Konstanten für die Entfeuchter-Preisregler-Integration."""
+
+DOMAIN = "entfeuchter_preisregler"
+
+CONF_HUMIDITY_ENTITY = "humidity_entity"
+CONF_SWITCH_ENTITY = "switch_entity"
+CONF_PRICE_ENTITY = "price_entity"
+
+DEFAULT_SETPOINT_MIN = 55.0
+DEFAULT_SETPOINT_MAX = 65.0
+DEFAULT_HYSTERESIS = 5.0
+DEFAULT_PRICE_LOW = 140.0
+DEFAULT_PRICE_HIGH = 190.0
+
+KEY_SETPOINT_MIN = "setpoint_min"
+KEY_SETPOINT_MAX = "setpoint_max"
+KEY_HYSTERESIS = "hysteresis"
+KEY_PRICE_LOW = "price_low"
+KEY_PRICE_HIGH = "price_high"
